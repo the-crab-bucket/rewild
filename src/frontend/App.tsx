b335165ee77976f4c643c568f78e61ad3c777668
Evelyn Nelson <evelynnelson000@gmail.com>
2025-02-01 16:48:18 -0500

    different env buttons

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -2,13 +2,14 @@ import { useCallback } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { RunKillButtons } from "./components/RunKillButtons";
 
 function App() {
   const launchServer = useCallback(async () => {
-    backend.runServer();
+    backend.runDevServer();
   }, []);
   const killServer = useCallback(async () => {
-    backend.killServer();
+    backend.killDevServer();
   }, []);
 
   return (
@@ -22,10 +23,8 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={launchServer}>Launch server</button>
-        <button onClick={killServer}>Kill server</button>
-      </div>
+      <RunKillButtons environment="dev" />
+      <RunKillButtons environment="prod" />
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
---
+++ src/frontend/components/RunKillButtons.tsx
@@ -0,0 +1,42 @@
+import { useCallback } from "react";
+
+export const RunKillButtons = (props: { environment: string }) => {
+  const { environment } = props;
+  const launchServer = useCallback(async () => {
+    if (environment === "dev") {
+      backend.runDevServer();
+    } else if (environment === "prod") {
+      backend.runProdServer();
+    }
+  }, [environment]);
+  const killServer = useCallback(async () => {
+    if (environment === "dev") {
+      backend.killDevServer();
+    } else if (environment === "prod") {
+      backend.killProdServer();
+    }
+  }, [environment]);
+
+  return (
+    <div className="card">
+      <button onClick={launchServer}>
+        Launch
+        {environment === "dev"
+          ? " local "
+          : environment === "prod"
+          ? " online "
+          : "unknown "}
+        server
+      </button>
+      <button onClick={killServer}>
+        Kill
+        {environment === "dev"
+          ? " local "
+          : environment === "prod"
+          ? " online "
+          : "unknown "}
+        server
+      </button>
+    </div>
+  );
+};
