cb36f69d31543f3528ad8af93926c9fc454e2ce0
Evelyn Nelson <evelynnelson000@gmail.com>
2025-02-02 21:24:35 -0500

    about page

--- src/frontend/components/About/AboutContainer.tsx
+++ src/frontend/components/About/AboutContainer.tsx
@@ -1,5 +1,15 @@
+import "./about.css";
+
 const AboutContainer = () => {
-  return <div>About</div>;
+  return (
+    <div className="about-container">
+      <h2>Rewild</h2>
+      <p className="about-text">
+        This is where a manifesto or something might go. Built by Esti, Evelyn,
+        and Natalie
+      </p>
+    </div>
+  );
 };
 
 export default AboutContainer;
